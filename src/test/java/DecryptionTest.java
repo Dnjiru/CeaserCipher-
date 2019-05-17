ddc60e313e7678ef3538c9f0af256a4e40a036df
Mwaniki <mr.mwaniki@gmail.com>
2019-05-17 17:53:48 +0300

    Decryption Test working

--- src/test/java/DecryptionTest.java
+++ src/test/java/DecryptionTest.java
@@ -1,10 +1,30 @@
 import org.junit.Test;
-
+import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class DecryptionTest {
 
     @Test
-    public void name() {
+    public void Decryption_lettershifted_String() {
+        Decryption testDecryption = new Decryption();
+        String expectedOutput = "a";
+        assertEquals(expectedOutput, testDecryption.decryption("c", 2));
     }
+
+    @Test
+    public void Decryption_wordshifted_String() {
+        Decryption testDecryption = new Decryption();
+        String expectedOutput = "cat";
+        assertEquals(expectedOutput, testDecryption.decryption("ecv", 2));
+    }
+
+    @Test
+    public void Decryption_sentenceshifted_String() {
+        Decryption testDecryption = new Decryption();
+        String expectedOutput = "javaprogrammingmustunderstand";
+        assertEquals(expectedOutput, testDecryption.decryption("lcxcrtqitcookpiowuvwpfgtuvcpf", 2));
+    }
+
+
+
 }
