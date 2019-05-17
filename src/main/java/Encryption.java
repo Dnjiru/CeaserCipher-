7e48a1b78d26da351a0b3de7fb5d2a323d3a00c6
Mwaniki <mr.mwaniki@gmail.com>
2019-05-17 08:38:57 +0300

    build the first test failed and passed

--- src/main/java/Encryption.java
+++ src/main/java/Encryption.java
@@ -1,6 +1,43 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
+
 
 public class Encryption {
 
 
+    //A Function that reads in a message and a key.
+    public String Encryption(String input, int key) {
+
+        //identify the character within the sentence. Find a character's location within the alphabet.
+        StringBuilder encrypted = new StringBuilder(input);
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String alphabet2 = alphabet.toLowerCase();
+        String keyedalphabet = alphabet.substring(key) +
+                alphabet.substring(0, key);
+
+        //a for loop that runs through every character within the message
+        for (int q = 0; q < encrypted.length(); q++) {
+            char currChar = encrypted.charAt(q);
+            int index = alphabet.indexOf(currChar);
+
+            //make sure the spot is a letter.
+            if (index != -1) {
+
+               //build a new sentence using the new characters in place of the original character.
+                char newChar = keyedalphabet.charAt(index);
+                encrypted.setCharAt(q, newChar);
+            }
+            index = alphabet2.indexOf(currChar);
+            if (index != -1) {
+
+                String keyedalaphabet2 =
+                        keyedalphabet.toLowerCase();
+                char newChar = keyedalaphabet2.charAt(index);
+                encrypted.setCharAt(q, newChar);
+            }
+        }
+        return encrypted.toString();
+
+    }
 }
+
+
--- src/test/java/EncryptionTest.java
+++ src/test/java/EncryptionTest.java
@@ -1,5 +1,18 @@
 import static org.junit.Assert.*;
+import org.junit.Test;
 
 public class EncryptionTest {
+    @Test
+    public void Encryption_lettershifted_String() {
+        Encryption testEncryption = new Encryption();
+        String expectedOutput = "c";
+        assertEquals(expectedOutput, testEncryption.Encryption("a", 2));
+    }
 
+    @Test
+    public void Encryption_wordshifted_String() {
+        Encryption testEncryption = new Encryption();
+        String expectedOutput = "ecv";
+        assertEquals(expectedOutput, testEncryption.Encryption("cat",2));
+    }
 }
