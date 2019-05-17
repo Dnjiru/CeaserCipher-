f7f3e444d73a1b566d41a53d1d74ad48c2d7254a
Mwaniki <mr.mwaniki@gmail.com>
2019-05-17 13:10:11 +0300

    Developed user interface for Encryption Working

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,6 +6,17 @@ public class App {
         Console myConsole = System.console();
         System.out.println("Enter the Message to be Encrypted");
 
+        String stringInput = myConsole.readLine();
+
+        System.out.println("Enter a shift Key");
+        String stringKey = myConsole.readLine();
+        int intKey = Integer.parseInt(stringKey);
+
+        Encryption testEncryption = new Encryption();
+        String encrypted = testEncryption.Encryption(stringInput,intKey);
+
+        System.out.println("Your Encryption is... " +  encrypted);
+
     }
 
 }
--- src/main/java/Decryption.java
+++ src/main/java/Decryption.java
@@ -1,4 +1,16 @@
+import java.util.Scanner;
 
 public class Decryption {
+
+    public String Decryption(String input, int key) {
+        StringBuilder decrypted = new StringBuilder(input);
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String alphabet2 = alphabet.toLowerCase();
+        String keyedalphabet = alphabet.substring(key) + alphabet.substring(0, key);
+
+        for(int q = 0; q < encrypted.length(); q++) {
+            char
+        }
+    }
 
 }
