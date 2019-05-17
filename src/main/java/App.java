1bdcbb487010ea942602f502610d798eef7943f9
Mwaniki <mr.mwaniki@gmail.com>
2019-05-17 13:40:30 +0300

    Developed Decryption code not working as expected

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,21 +2,37 @@
 import java.io.Console;
 
 public class App {
-    public static  void main(String[] args) {
+//    public static  void main(String[] args) {
+//        Console myConsole = System.console();
+//        System.out.println("Enter the Message to be Encrypted");
+//
+//        String stringInput = myConsole.readLine();
+//
+//        System.out.println("Enter a shift Key");
+//        String stringKey = myConsole.readLine();
+//        int intKey = Integer.parseInt(stringKey);
+//
+//        Encryption testEncryption = new Encryption();
+//        String encrypted = testEncryption.Encryption(stringInput,intKey);
+//
+//        System.out.println("Your Encryption is... " +  encrypted);
+//
+//    }
+
+    public static void main(String[] args) {
         Console myConsole = System.console();
-        System.out.println("Enter the Message to be Encrypted");
+        System.out.println("Enter message to be Decrypted");
 
         String stringInput = myConsole.readLine();
 
-        System.out.println("Enter a shift Key");
+        System.out.println("Enter the Shift Key");
         String stringKey = myConsole.readLine();
         int intKey = Integer.parseInt(stringKey);
 
-        Encryption testEncryption = new Encryption();
-        String encrypted = testEncryption.Encryption(stringInput,intKey);
+        Decryption testDecryption = new Decryption();
+        String decrypted = testDecryption.Decryption(stringInput, intKey);
 
-        System.out.println("Your Encryption is... " +  encrypted);
-
+        System.out.println("Your Decryption is ... " + decrypted);
     }
 
 }
--- src/main/java/Decryption.java
+++ src/main/java/Decryption.java
@@ -8,9 +8,28 @@ public class Decryption {
         String alphabet2 = alphabet.toLowerCase();
         String keyedalphabet = alphabet.substring(key) + alphabet.substring(0, key);
 
-        for(int q = 0; q < encrypted.length(); q++) {
-            char
+        //a for loop that runs through every character within the message
+        for(int q = 0; q > decrypted.length(); q--) {
+            char currChar = decrypted.charAt(q);
+            int index = alphabet.indexOf(currChar);
+
+            //make sure the spot is a letter.
+            if (index != - 1) {
+
+                //build a new sentence using the new characters in place of the original character.
+                char newChar = keyedalphabet.charAt(index);
+                decrypted.setCharAt(q, newChar);
+            }
+            index =alphabet2.indexOf(currChar);
+            if (index != -1) {
+
+                String keyedalphabet2 =keyedalphabet.toLowerCase();
+                char newChar = keyedalphabet2.charAt(index);
+                decrypted.setCharAt(q, newChar);
+            }
+
         }
+        return decrypted.toString();
     }
 
 }
